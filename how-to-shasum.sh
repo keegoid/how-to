4ec318425dae61f33bae9c8a695fbbf5acfd0701
Keegan Mullaney <keegan@kmauthorized.com>
2021-03-06 11:39:15 -0800

    chore: add ledger command

--- how-to-shasum.sh
+++ how-to-shasum.sh
@@ -5,3 +5,6 @@ sha512sum logclean.sh > SHA512SUMS
 
 # verify shasum for a file
 sha512sum -c SHA512SUMS
+
+# verify ledger AppImage
+sha512sum ledger-live-desktop-2.23.0-linux-x86_64.AppImage | cut -d' ' -f1 | xxd -r -p | base64 -w 0 | grep $(cat latest-linux.yml | grep -E ^\(sha512\) | cut -d' ' -f2) && echo valid || echo invalid
